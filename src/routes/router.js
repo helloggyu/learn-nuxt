268b579f95013c227c912927b19d400e06025387
helloggyu <rbwls111822@gmail.com>
2022-07-20 03:58:56 +0900

    :sparkles: route 모드 추가

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,10 +3,13 @@ import VueRouter from 'vue-router';
 import NewsView from '../views/NewsView.vue';
 import AskView from '../views/AskView.vue';
 import JobsView from '../views/JobsView.vue';
+import ItemView from '../views/ItemView.vue';
+import UserView from '../views/UserView.vue';
 
 Vue.use(VueRouter);
 
  const router = new VueRouter({
+   mode:'history',
   routes:[
     {
       path:'/',
@@ -24,6 +27,14 @@ Vue.use(VueRouter);
       path:'/jobs',
       component:JobsView,
     },
+    {
+      path:'/item',
+      component:ItemView,
+    },
+    {
+      path:'/user',
+      component:UserView,
+    }
   ],
 });
 
