00f8e8f33ad8909a25367d4741e54bccde845bf2
helloggyu <rbwls111822@gmail.com>
2022-07-21 16:45:55 +0900

    :sparkles: user api 호출

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>news</div>
+  <div><div v-for="(user,i) in users" :key="i">{{user.title}}</div></div>
 </template>
 
 <script>
+import axious from 'axios'
 export default {
-
+  data(){
+    return{
+      users:[]
+    }
+  },
+  created(){
+    var vm=this;
+    axious.get('https://api.hnpwa.com/v0/news/1.json')
+    .then(response=>vm.users=response.data)
+    .catch(error=>console.log(error))
+  }
 }
+//	https://api.hnpwa.com/v0/news/1.json
 </script>
 
 <style>
