bc0a56d63007fca06252dc041ff6a6957d54e0fe
helloggyu <rbwls111822@gmail.com>
2022-07-23 17:02:32 +0900

    ✨ aks,jobs api 호출

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>ask</div>
+  <div>
+    <div><div v-for="(ask,i) in asks" :key="i">{{ask.title}}</div></div>
+  </div>
 </template>
 
 <script>
+import { fetchAskList } from '../api/index'
 export default {
-
+  data(){
+    return{
+      asks:[]
+    }
+  },
+  created(){
+    var vm=this;
+    fetchAskList()
+    .then(response=>vm.asks=response.data)
+    .catch(error=>console.log(error))
+  }
 }
 </script>
 
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>jobs</div>
+  <div><div v-for="(job,i) in jobs" :key="i">{{job.title}}</div></div>
 </template>
 
 <script>
+import { fetchJobsList } from '../api/index'
 export default {
-
+  data(){
+    return{
+      jobs:[]
+    }
+  },
+  created(){
+    var vm=this;
+   fetchJobsList()
+    .then(response=>vm.jobs=response.data)
+    .catch(error=>console.log(error))
+  }
+  
 }
 </script>
 
