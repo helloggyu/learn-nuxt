fc8420147a598418cec51bf4d9259a22c6c0e8f7
helloggyu <rbwls111822@gmail.com>
2022-07-26 21:47:58 +0900

    ♻️ user 상세 컴포넌트 slot 처리

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="item-view">
     <section>
-      <!-- 질문상세 -->
-      <div class="item-view__container">
-        <div>
-          <i class="fas fa-user"></i>
-        </div>
-        <div class="item-view__discription">
-          <router-link :to="`/user/${fetchedItem.user}`">
-            {{fetchedItem.user}}
-          </router-link>
-          <div class="item-view__time">{{fetchedItem.time_ago}}</div>
-        </div>
-      </div>
+      <UserDetail :info="fetchedItem">
+        <template #userInfo>
+            <router-link :to="`/user/${fetchedItem.user}`">
+              {{fetchedItem.user}}
+            </router-link>
+        </template>
+        <template #time>
+           {{fetchedItem.time_ago}}
+        </template>
+      </UserDetail>
       <h2>{{fetchedItem.title}}</h2>
-      
     </section>
-
     <section>
       <!-- 질문 댓글 -->
       <div v-html="fetchedItem.content"/>
@@ -27,8 +23,11 @@
 
 <script>
 import {mapGetters} from 'vuex'
-
+import UserDetail from '../components/UserDetail.vue'
 export default {
+  components:{
+    UserDetail,
+  },
   computed:{
       ...mapGetters(['fetchedItem'])
   },
@@ -44,23 +43,5 @@ export default {
 .item-view{
   padding: 0.5rem;
   color:#828282;
-  word-wrap: break-word;
-  word-break:break-word;
-  white-space: pre-line; 
-  font-size: 1rem;
-  &__time{
-     font-size: 0.7rem;
-  }
-}
-.item-view__container{
-  display: flex;
-  align-items: center;
-  padding: 0.5rem;
-}
-.fa-user{
-  font-size: 2.5rem;
-}
-.item-view__discription{
-  padding-left: 8px;
 }
 </style>
