c584426372e6ce280eec37fe4d968d03ca7bbbac
helloggyu <rbwls111822@gmail.com>
2022-07-28 01:49:11 +0900

    🐛 router 충돌

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router';
 import ItemView from '../views/ItemView.vue';
 import UserView from '../views/UserView.vue';
-//import createListView from '../views/CreateListView';
+import createListView from '../views/CreateListView';
 import NewsView from '../views/NewsView.vue';
-import AskView from '../views/AskView.vue';
-import JobsView from '../views/JobsView.vue';
+//import AskView from '../views/AskView.vue';
+//import JobsView from '../views/JobsView.vue';
 
 
 Vue.use(VueRouter);
@@ -26,14 +26,14 @@ Vue.use(VueRouter);
     {
       path:'/ask',
       name:'ask',
-      component:AskView,
-     // component:createListView('AskView'),
+     // component:AskView,
+      component:createListView('AskView'),
     },
     {
       path:'/jobs',
       name:'jobs',
-      component:JobsView,
-      //component:createListView('JobsView'),
+      //component:JobsView,
+      component:createListView('JobsView'),
     },
     {
       path:'/item/:id',
